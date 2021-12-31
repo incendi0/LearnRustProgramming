81d0fdf7410fec5d0b0d01656e3976dae9971b40
incendi0 <69083424+incendi0@users.noreply.github.com>
2021-12-31 15:05:51 +0800

    Update lib.rs

--- cl-rs/src/lib.rs
+++ cl-rs/src/lib.rs
@@ -1,7 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#![allow(dead_code)]
+
+mod divide_conquer;
+mod dp;
+mod greedy;
+mod priority_queue;
+mod sort;
+mod tree;
+mod graph;
