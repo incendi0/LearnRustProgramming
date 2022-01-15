b22032525ec2542f7c650fdf7d08e949dae1d65f
incendi0 <1184813573@qq.com>
2022-01-15 22:59:26 +0800

    macros

---
+++ leetcode-rs/src/p1716_calculate_money_in_leetcode_bank.rs
@@ -0,0 +1,23 @@
+struct Solution;
+
+impl Solution {
+    pub fn total_money(n: i32) -> i32 {
+        let first_week_sum = 28;
+        let m = n / 7;
+        let h = n % 7;
+        (first_week_sum + (m - 1) * 7 + first_week_sum) * m / 2 + (1 + m + 1 + m + h - 1) * h / 2
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::Solution;
+
+    #[test] 
+    fn total_money_works() {
+        assert_eq!(Solution::total_money(4), 10);
+        assert_eq!(Solution::total_money(10), 37);
+        assert_eq!(Solution::total_money(20), 96);
+    }
+}
