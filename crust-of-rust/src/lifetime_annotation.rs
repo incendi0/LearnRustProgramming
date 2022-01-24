be2c6962acc10c9cb86acfb2d5eee67b4cffdf7c
incendi0 <69083424+incendi0@users.noreply.github.com>
2022-01-24 13:26:20 +0800

    use Delimiter trait_v3

--- crust-of-rust/src/lifetime_annotation.rs
+++ crust-of-rust/src/lifetime_annotation.rs
@@ -1,10 +1,14 @@
-pub struct StrSplit<'a, 'b> {
+pub struct StrSplit<'a, Delimiter> {
     remainder: Option<&'a str>,
-    delimiter: &'b str,
+    delimiter: Delimiter,
 }
 
-impl<'a, 'b> StrSplit<'a, 'b> {
-    pub fn new(haystack: &'a str, delimiter: &'b str) -> Self {
+pub trait Delimiter {
+    fn find_next(&self, s: &str) -> Option<(usize, usize)>;
+}
+
+impl<'a, Delimiter> StrSplit<'a, Delimiter> {
+    pub fn new(haystack: &'a str, delimiter: Delimiter) -> Self {
         StrSplit { 
             remainder: Some(haystack),
             delimiter,
@@ -12,25 +16,34 @@ impl<'a, 'b> StrSplit<'a, 'b> {
     }
 }
 
-impl<'a, 'b> Iterator for StrSplit<'a, 'b> {
+impl<'a, D: Delimiter> Iterator for StrSplit<'a, D> {
     type Item = &'a str;
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(ref mut remainder) = self.remainder {
-            if let Some(delim_idx) = remainder.find(self.delimiter) {
-                let until_delimiter = &remainder[..delim_idx];
-                *remainder = &remainder[delim_idx + self.delimiter.len()..];
-                Some(until_delimiter)
-            } else {
-                self.remainder.take()
-            }
+        let remainder = self.remainder.as_mut()?;
+        if let Some((delimt_start, delim_end)) = self.delimiter.find_next(remainder) {
+            let until_delimiter = &remainder[..delimt_start];
+            *remainder = &remainder[delim_end..];
+            Some(until_delimiter)
         } else {
-            None
+            self.remainder.take()
         }
     }
 }
 
+impl Delimiter for &str {
+    fn find_next(&self, s: &str) -> Option<(usize, usize)> {
+        s.find(self).map(|start| (start, start + self.len()))
+    }
+}
+
+impl Delimiter for char {
+    fn find_next(&self, s: &str) -> Option<(usize, usize)> {
+        s.char_indices().find(|(_, c)| c == self).map(|(start, _)| (start, start + self.len_utf8()))
+    }
+}
+
 fn until_char(s: &str, c: char) -> &str {
-    StrSplit::new(s, &c.to_string()).next().unwrap()
+    StrSplit::new(s, c).next().expect("need at least one match result")
 }
 
 
@@ -40,12 +53,20 @@ mod tests {
 
     #[test]
     fn iterator_works() {
-        let mut ss = StrSplit::new("a b c d e ", " ");
-        assert_eq!(ss.next(), Some("a"));
-        assert_eq!(ss.next(), Some("b"));
-        assert_eq!(ss.next(), Some("c"));
-        assert_eq!(ss.next(), Some("d"));
-        assert_eq!(ss.next(), Some("e"));
+        let mut ss = StrSplit::new("张 王 李 赵 刘 ", '李');
+        assert_eq!(ss.next(), Some("张 王 "));
+        assert_eq!(ss.next(), Some(" 赵 刘 "));
+        assert_eq!(ss.next(), None);
+    }
+
+    #[test]
+    fn iterator_works_2() {
+        let mut ss = StrSplit::new("张 王 李 赵 刘 ", ' ');
+        assert_eq!(ss.next(), Some("张"));
+        assert_eq!(ss.next(), Some("王"));
+        assert_eq!(ss.next(), Some("李"));
+        assert_eq!(ss.next(), Some("赵"));
+        assert_eq!(ss.next(), Some("刘"));
         assert_eq!(ss.next(), Some(""));
         assert_eq!(ss.next(), None);
     }
@@ -53,5 +74,7 @@ mod tests {
     #[test]
     fn until_char_works() {
         assert_eq!(until_char("hello world", 'o'), "hell");
+        assert_eq!(until_char("y̆es", 'y'), "");
+        // assert_eq!(until_char("y̆es", "y̆"), "");
     }
 }
