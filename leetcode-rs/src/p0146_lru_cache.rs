3982220109d8df7f1c7756d88f53f68475b41f9c
incendi0 <69083424+incendi0@users.noreply.github.com>
2022-02-11 20:11:38 +0800

    Update p0146_lru_cache.rs

--- leetcode-rs/src/p0146_lru_cache.rs
+++ leetcode-rs/src/p0146_lru_cache.rs
@@ -41,15 +41,15 @@ impl LRUCache {
     }
 
     fn get(&mut self, key: i32) -> i32 {
-        if let Some(link) = self.cached_map.remove(&key) {
-            if let Some(node) = link {
-                let value = node.borrow().value;
-                self.delete(&node);
-                self.push_front(&node);
-                value
-            } else {
-                -1
-            }
+        if !self.cached_map.contains_key(&key) {
+            return -1;
+        }
+        if let Some(node) = self.cached_map.get(&key).unwrap() {
+            let node = Rc::clone(node);
+            let value = node.borrow().value;
+            self.delete(&node);
+            self.push_front(node);
+            value
         } else {
             -1
         }
@@ -57,57 +57,59 @@ impl LRUCache {
 
     fn put(&mut self, key: i32, value: i32) {
         if self.cached_map.contains_key(&key) {
-            let node = self.cached_map.remove(&key).unwrap().unwrap();
+            let node = Rc::clone(self.cached_map.get(&key).unwrap().as_ref().unwrap());
             self.delete(&node);
         } else {
-            if self.cached_map.len() == self.capacity as usize {
+            if self.cached_map.len() >= self.capacity as usize {
                 let node = Rc::clone(self.tail.as_ref().unwrap());
                 self.delete(&node);
-            } 
+            }
         }
         let node = Node::new(key, value);
-        self.push_front(&node);
+        self.push_front(Rc::clone(&node));
         self.cached_map.insert(key, Some(node));
     }
 
-    fn push_front(&mut self, node: &Rc<RefCell<Node>>) {
-        assert!(node.borrow().prev.is_none());
-        assert!(node.borrow().next.is_none());
-        let node = Rc::clone(node);
-        let key = node.borrow().key;
+    fn push_front(&mut self, node: Rc<RefCell<Node>>) {
         match self.head.take() {
             None => {
                 self.head = Some(node.clone());
                 self.tail = Some(node.clone());
-            },
+            }
             Some(old_head) => {
                 old_head.borrow_mut().prev = Some(Rc::clone(&node));
                 node.borrow_mut().next = Some(old_head);
                 self.head = Some(Rc::clone(&node));
             }
         }
+        let key = node.borrow().key;
         self.cached_map.insert(key, Some(node));
     }
 
     fn delete(&mut self, node: &Rc<RefCell<Node>>) {
-        match (Rc::clone(node).borrow().prev.as_ref(), Rc::clone(node).borrow().next.as_ref()) {
+        match (
+            Rc::clone(node).borrow().prev.as_ref(),
+            Rc::clone(node).borrow().next.as_ref(),
+        ) {
             (None, None) => {
                 self.head = None;
                 self.tail = None;
-            },
+            }
             (None, Some(next)) => {
                 self.head = Some(Rc::clone(next));
                 next.borrow_mut().prev = None;
-            },
+            }
             (Some(prev), None) => {
                 self.tail = Some(Rc::clone(prev));
                 prev.borrow_mut().next = None;
-            },
+            }
             (Some(prev), Some(next)) => {
                 next.borrow_mut().prev = Some(Rc::clone(prev));
                 prev.borrow_mut().next = Some(Rc::clone(next));
-            },
+            }
         }
+        node.borrow_mut().next = None;
+        node.borrow_mut().prev = None;
         self.cached_map.remove(&node.borrow().key);
     }
 }
@@ -119,13 +121,12 @@ mod tests {
     fn main_case() {
         let mut cache = LRUCache::new(2);
         cache.put(1, 1);
-        cache.put(1, 2);
-        assert_eq!(cache.get(1), 2);
         cache.put(2, 2);
+        assert_eq!(cache.get(1), 1);
         cache.put(3, 3);
-        assert_eq!(cache.get(1), -1);
-        cache.put(4, 4);
         assert_eq!(cache.get(2), -1);
+        cache.put(4, 4);
+        assert_eq!(cache.get(1), -1);
         assert_eq!(cache.get(3), 3);
         assert_eq!(cache.get(4), 4);
     }
